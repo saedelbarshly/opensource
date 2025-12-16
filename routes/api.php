b988fe51c9043100726abd54f77072fe8ea5441b
Elsaaed Elbarshly <saedelbarshly@gmail.com>
2025-12-16 15:08:53 +0200

    docker step 2 add phpmyadmin

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::get('test', function () {
+    return response()->json([
+        'message' => 'Success 🤑',
+    ]);
+});
