2b5447e0d63eb0c72270f9db6df5f6eda1c21142
Elsaaed Elbarshly <saedelbarshly@gmail.com>
2026-03-10 15:55:16 +0200

    databasea

--- config/database.php
+++ config/database.php
@@ -44,11 +44,25 @@ return [
         ],
 
         'mysql' => [
+
+            'read' => [
+                'host' => [
+                    env('DB_READ_HOST', '127.0.0.1'),
+                ],
+                'database' => env('DB_READ_DATABASE', 'opensource_read'),
+            ],
+
+            'write' => [
+                'host' => [
+                    env('DB_WRITE_HOST', '127.0.0.1'),
+                ],
+                'database' => env('DB_WRITE_DATABASE', 'opensource'),
+            ],
             'driver' => 'mysql',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            // 'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'laravel'),
+            // 'database' => env('DB_DATABASE', 'laravel'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
